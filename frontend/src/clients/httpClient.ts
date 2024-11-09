4e54500ffdf0e80cf3c475bf0d12ff778085c609
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-11-09 12:52:07 +0000

    feat(frontend): generate backend client

--- frontend/src/clients/httpClient.ts
+++ frontend/src/clients/httpClient.ts
@@ -1,10 +1,7 @@
 // Warning: use `import type`, rather than `import` to avoid accidentally importing backend packages that are transitively imported. This would cause the application to fail to start.
 import { env } from "../env";
-import { hcWithType } from "@repo/backend/httpClient";
 
-export const client = hcWithType(env.backendHttpUrl);
+import createClient from "openapi-fetch";
+import type {paths} from '../codegen/client';
 
-// Less efficient example, but works without needing to build the backend (more type inference at runtime)
-// import {hc} from 'hono/client';
-// import type {AppType} from '@repo/backend';
-// export const client = hc<AppType>(env.backendHttpUrl);
+export const client = createClient<paths>({ baseUrl: env.backendHttpUrl });
