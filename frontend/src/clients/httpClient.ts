583ec032d9f192b503ef12e41c8ac1072fa6861d
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-11-09 12:55:10 +0000

    docs(frontend): add examples

--- frontend/src/clients/httpClient.ts
+++ frontend/src/clients/httpClient.ts
@@ -5,3 +5,27 @@ import createClient from "openapi-fetch";
 import type {paths} from '../codegen/client';
 
 export const client = createClient<paths>({ baseUrl: env.backendHttpUrl });
+
+
+//Examples
+// const reply = await client.POST("/uploadfiles/", {
+//     body: {files: []}
+// })
+
+
+// // 
+// const mutation = useMutation({
+//     mutationFn: async (files: File[]) => {
+//         const reply = await client.POST("/uploadfiles/", {
+//             body: {files}
+//         })
+//         return reply;
+//     }
+// })
+
+// if (mutation.isLoading) {
+//     return <div>Loading...</div>
+// }
+// if (mutation.data) {
+//     return <div>Success!</div>
+// }
