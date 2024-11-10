07b018114f1ae4d22d8e2ab5db71db66eb77d1f1
3miki <90838632+3miki@users.noreply.github.com>
2024-11-10 13:23:54 +0000

    update translation prompt and model

--- python-backend/src/openai_summary.py
+++ python-backend/src/openai_summary.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 
 load_dotenv()
 
+openaiModel = "gpt-4-turbo"
 
 class TranscriptMessage(BaseModel):
     source: Literal["ai", "user"]
@@ -16,7 +17,7 @@ openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 # image summarisation
 def create_summary_from_images_and_metadata(image_urls: list[str], travellers: list[str], trip_description: str, transcript_messages: list[TranscriptMessage]) -> str:
     prompt = """
-    You are a summariser that summarises travel photos in a personal manner with a warm positive tone with a range of emotions. Your job is to examine photos that are uploaded to you and make good descriptive summaries of their travel photos. It should feel like a story, reminiscent, maybe poignant when necessary, maybe happy or sad depending on the content of the photos uploaded, you can use the metadata from the photos to determine location where possible but you should also just be able to deduce the location just from the contents of the photos i.e they're photographed next a famous well known landmark etc. You should attempt to create a catchy script including the city name if possible and generate a summary of their trip in 40 seconds speech length that can be used for a narration of a travel video composed of their pictures. The narration should use traveller name or first person pronouns such as "I" or "We"  depending on the travellers but never use second person pronouns such as "They" or "She". The summary will be read by a text to speech service so should be formatted as it is pronounced.
+    You are a summariser that summarises travel photos in a personal manner with a warm positive tone with a range of emotions. Your job is to examine photos that are uploaded to you and make good descriptive summaries of their travel photos. It should feel like a story, reminiscent, maybe poignant when necessary, maybe happy or sad depending on the content of the photos uploaded, you can use the metadata from the photos to determine location where possible but you should also just be able to deduce the location just from the contents of the photos i.e they're photographed next a famous well known landmark etc. You should attempt to create a catchy script including the city name if possible, and generate a summary of their trip totally in 30 seconds speech length that can be used for a narration of a travel video composed of their pictures. The narration should use traveller name or first person pronouns such as "I" or "We"  depending on the travellers but do not use third person pronouns such as "They" or "She" in the narration. The summary will be read by a text to speech service so should be formatted as it is pronounced. Do not include any captions, e.g. [Opening music plays].
     Here are all the images that you need to create one single summary for:
     """ + "\n".join([image_url for image_url in image_urls]) + "\n"
 
@@ -39,7 +40,7 @@ def create_summary_from_images_and_metadata(image_urls: list[str], travellers: l
                 "content": prompt,
             }
         ],
-        model="gpt-3.5-turbo",
+        model=openaiModel,
     )
 
     summary_content = summary.choices[0].message.content
@@ -48,7 +49,7 @@ def create_summary_from_images_and_metadata(image_urls: list[str], travellers: l
 
 
 def translate_summary(summary_content: str):
-    prompt = """Translate the summary into Japansese. The translated script will be read by a text to speech service so should be formatted as it is pronounced as well as the length of the speech has to be the same as original summary when it is read. """ + summary_content + "\n"
+    prompt = """Translate the text into Japanese. It should sound like travellers are the narrators of the story.""" + summary_content + "\n"
     translation = openai_client.chat.completions.create(
         messages=[
             {
@@ -56,7 +57,7 @@ def translate_summary(summary_content: str):
                 "content": prompt,
             }
         ],
-        model="gpt-3.5-turbo",
+        model=openaiModel,
     )
 
     translation_content = translation.choices[0].message.content
