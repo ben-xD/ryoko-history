86b16d2b91dda5840bdfa656b55c9599aacfa1a7
3miki <90838632+3miki@users.noreply.github.com>
2024-11-10 10:51:35 +0000

    improve summary_from_images prompt

--- python-backend/src/openai_summary.py
+++ python-backend/src/openai_summary.py
@@ -23,9 +23,14 @@ openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 # image summarisation
 def create_summary_from_images_and_metadata(images: list[str], travellers: list[str], trip_description: str):
     prompt = """
-    You are a summariser that summarises travel photos in a personal manner with a warm positive tone with a range of emotions. Your job is to examine photos that are uploaded to you and make good descriptive summaries of their travel photos. It should feel like a story, reminiscent, maybe poignant when necessary, maybe happy or sad depending on the content of the photos uploaded, you can use the metadata from the photos to determine location where possible but you should also just be able to deduce the location just from the contents of the photos i.e they're photographed next a famous well known landmark etc. The summary for each image should take less than 2 seconds to speak.
+    You are a summariser that summarises travel photos in a personal manner with a warm positive tone with a range of emotions. Your job is to examine photos that are uploaded to you and make good descriptive summaries of their travel photos. It should feel like a story, reminiscent, maybe poignant when necessary, maybe happy or sad depending on the content of the photos uploaded, you can use the metadata from the photos to determine location where possible but you should also just be able to deduce the location just from the contents of the photos i.e they're photographed next a famous well known landmark etc. You should attempt to create a catchy script including the city name if possible and generate a summary of their trip in 40 seconds speech length that can be used for a narration of a travel video composed of their pictures. The narration should use singular such as "I" or "We" depending on the main travellers. The summary will be read by a text to speech service so should be formatted as it is pronounced.
     Here are multiple images that you need to summarise:
-    """ + "\n".join([f"- Image {i+1}: {image}" for i, image in enumerate(images)])
+    """ + "\n".join([image for image in images])
+
+    # prompt = """
+    # You are a summariser that summarises travel photos in a personal manner with a warm positive tone with a range of emotions. Your job is to examine photos that are uploaded to you and make good descriptive summaries of their travel photos. It should feel like a story, reminiscent, maybe poignant when necessary, maybe happy or sad depending on the content of the photos uploaded, you can use the metadata from the photos to determine location where possible but you should also just be able to deduce the location just from the contents of the photos i.e they're photographed next a famous well known landmark etc. The summary for each image should take less than 2 seconds to speak.
+    # Here are multiple images that you need to summarise:
+    # """ + "\n".join([f"- Image {i+1}: {image}" for i, image in enumerate(images)])
 
     # print("prompt:", prompt)
 
