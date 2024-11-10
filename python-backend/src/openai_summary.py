05c3de9b40e76eeaff4c9918d30d051657465f27
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-11-10 11:30:54 +0000

    feat: add transcript message to openai function

--- python-backend/src/openai_summary.py
+++ python-backend/src/openai_summary.py
@@ -1,8 +1,15 @@
 import os
+from typing import Literal
 from openai import OpenAI
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv()
+
+
+class TranscriptMessage(BaseModel):
+    source: Literal["ai", "user"]
+    message: str
 
 openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
@@ -21,7 +28,7 @@ openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 # print(summary_content)
 
 # image summarisation
-def create_summary_from_images_and_metadata(images: list[str], travellers: list[str], trip_description: str):
+def create_summary_from_images_and_metadata(images: list[str], travellers: list[str], trip_description: str, transcript_messages: list[TranscriptMessage]) -> str:
     prompt = """
     You are a summariser that summarises travel photos in a personal manner with a warm positive tone with a range of emotions. Your job is to examine photos that are uploaded to you and make good descriptive summaries of their travel photos. It should feel like a story, reminiscent, maybe poignant when necessary, maybe happy or sad depending on the content of the photos uploaded, you can use the metadata from the photos to determine location where possible but you should also just be able to deduce the location just from the contents of the photos i.e they're photographed next a famous well known landmark etc. You should attempt to create a catchy script including the city name if possible and generate a summary of their trip in 40 seconds speech length that can be used for a narration of a travel video composed of their pictures. The narration should use singular such as "I" or "We" depending on the main travellers. The summary will be read by a text to speech service so should be formatted as it is pronounced.
     Here are multiple images that you need to summarise:
